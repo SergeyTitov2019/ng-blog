18919b6008f8e0e47bf7be84e7626d9bf83af4c9
Titov Sergey <sergeyntitov@gmail.com>
2024-04-19 22:50:31 +0300

    lesson [145] add quill

--- src/app/admin/create/create.component.ts
+++ src/app/admin/create/create.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {Post} from "../../shared/interfaces/post";
+import {PostService} from "../../shared/services/post.service";
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit{
 
+  form!: FormGroup
+
+  constructor(private postService: PostService) {
+  }
+
+  ngOnInit() {
+    this.form = new FormGroup<any>({
+      title: new FormControl(null, Validators.required),
+      text: new FormControl(null, Validators.required),
+      author: new FormControl(null, Validators.required),
+    })
+  }
+
+  submit(){
+    if(this.form.invalid){
+      return
+    }
+    const post: Post = {
+      title: this.form.value.title,
+      author: this.form.value.author,
+      text: this.form.value.text,
+      date: new Date(),
+    }
+    console.log(post);
+    this.postService.create(post).subscribe(() => {
+      this.form.reset()
+    })
+  }
 }
