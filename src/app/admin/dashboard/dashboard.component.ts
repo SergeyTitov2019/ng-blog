3e30a8ea827751c003192a3fb1ef7e0cba0e2ea8
Titov Sergey <sergeyntitov@gmail.com>
2024-04-20 15:49:43 +0300

    lesson [147] dashboard

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import {AuthService} from "../../shared/services/auth.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { PostService } from "../../shared/services/post.service";
+import { Post } from "../../shared/interfaces/post";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit, OnDestroy{
 
-  constructor(private authService: AuthService) {
+  posts: Post[] = []
+  postSub!: Subscription
+
+  constructor(private postService: PostService) {
   }
 
+  ngOnInit() {
+    this.postService.getAll().subscribe((posts: Post[]) => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy() {
+    if(this.postSub){
+      this.postSub.unsubscribe()
+    }
+  }
+
+  onDelete(id: string | undefined) {
+    console.log(id)
+  }
 }
